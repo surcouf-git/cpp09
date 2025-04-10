5d95a33c1cbd4247c35e5ce313dbceb843bb52b9
Robert Surcouf <mvannest@student.42lehavre.fr>
2025-04-10 19:04:50 +0200

    add:ex01:conflit <stack> char/char*

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -13,19 +13,13 @@ void	RPN::_parse_(std::string &line) {
 	int			digit = 0;
 	std::string	operators = "/*-+";
 
-	(void)sign;
-	(void)digit;
-	for (size_t i = 0; line.length(); i++) {
-		// if (i % 2 != 0)
-		// 	std::cout	<< i << "-{" << line.at(i) << "} ";
-		if (i != 0 && i % 2 != 0)
-			if (line.at(i) != ' ') {
-				// std::cout << "{" << line.at(i) << "} ";
-				throw (std::runtime_error("Error"));
-			}
-	}
 	if (line.length() < 3)
 		throw (std::runtime_error("Error"));
+	for (size_t i = 0; i < line.length(); i++) {
+		if (i != 0 && i % 2 != 0)
+			if (line.at(i) != ' ')
+				throw (std::runtime_error("Error"));
+	}
 	for (size_t i = 0; i < line.length(); i++) {
 		if (std::isdigit(line.at(i))) {
 			digit++;
@@ -41,44 +35,74 @@ void	RPN::_parse_(std::string &line) {
 }
 
 void	RPN::parse_fill_do(std::string &line) {
-	int			count = 0;
+	int		count = 0;
 	(void)count;
 	std::string	operators = "/*-+ ";
 
 	this->_parse_(line);
-	// for (int i = (line.length() - 1); i >= 0; i--) {
-	// 	count++;
-	// 	if (operators.find(line.at(i)) != std::string::npos) {
-	// 		if (line.at(i) != ' ') {
-	// 			if (i < 2)
-	// 				throw (std::runtime_error("Error"));
-	// 			count++;
-	// 			this->_stack.push(line.at(i));
-	// 		}
-	// 		continue ;
-	// 	}
-	// 	if (std::isdigit(line.at(i)))
-	// 		this->_stack.push(line.at(i));
-	// 	else
-	// 		throw (std::runtime_error("Error"));
-	// }
-	// this->_do_();
+	// std::cout << chars << '\n';
+	for (int i = (line.length() - 1); i >= 0; i--) {
+		count++;
+		if (operators.find(line.at(i)) != std::string::npos) {
+			if (line.at(i) != ' ') {
+				if (i < 2)
+					throw (std::runtime_error("Error"));
+				count++;
+				this->_stack.push(chars);
+			}
+			continue ;
+		}
+		if (std::isdigit(line.at(i)))
+			this->_stack.push(chars);
+		else
+			throw (std::runtime_error("Error"));
+	}
+	this->_do_();
 	// for (;!this->_stack.empty();) {
-	// 	std::cout	<< this->_stack.top() << '\n';
+	// 	std::cout	<< this->_stack.top() << "";
 	// 	this->_stack.pop();
 	// }
 }
 
-void	RPN::_do_(void) {
-	// int		result;
-	// char	n1, n2, sign;
+void	RPN::_swap_(const char c) {
+	int	n1, n2 = 0;
 
+	n1 = this->_result.top();
+	std::cout	<< ">" << this->_result.top() << "<\n";
+	this->_result.pop();
+	std::cout	<< ">" << this->_result.top() << "<\n";
+	n2 = this->_result.top();
+	this->_result.pop();
+	std::cout	<< ">" << n1 << ' ' << n2 << "<\n";
+	if (c == '/')
+		this->_result.push(n1 / n2);
+	else if (c == '*')
+		this->_result.push(n1 * n2);
+	this->_stack.pop();
+}
+
+void	RPN::_do_(void) {
+
+	std::cout	<< static_cast<int>(std::atol(this->_stack.top())) << '\n';
+	this->_stack.pop();
+	// std::string	test(&this->_stack.top());
+	std::cout << ">" << this->_stack.top() << '\n';
+	std::cout	<< std::atol(this->_stack.top()) << '\n';
+	this->_stack.pop();
+	std::cout	<< &this->_stack.top() << '\n';
+	exit (0);
 	for (;!this->_stack.empty();) {
-		std::cout	<< this->_stack.top() << '>';
-		if (std::isdigit(this->_stack.top())) {
-			this->_result.push(static_cast<unsigned int>(this->_stack.top()));
+		if (this->_stack.top()[0] == '/')
+			this->_swap_('/');
+		else if (this->_stack.top()[0] == '*')
+			this->_swap_('*');
+		else {
+			this->_result.push(static_cast<int>(std::atol(this->_stack.top())));
 			this->_stack.pop();
-		} else {
 		}
+	}
+	for (;!this->_result.empty();) {
+		std::cout	<< this->_result.top() << " ";
+		this->_result.pop();
 	}
 }
--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -3,13 +3,20 @@
 #include <iostream>
 #include <stack>
 
+#include <iostream>
+#include <limits>
+#include <cmath>
+#include <cstdlib>
+#include <iomanip>
+
 class RPN
 {
 	private:
-		std::stack<char>				_stack;
-		std::stack<unsigned int>		_result;
+		std::stack<char*>	_stack;
+		std::stack<int>		_result;
 
 		void	_do_(void);
+		void	_swap_(const char c);
 		void	_parse_(std::string &line);
 
 	public:
